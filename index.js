5114416cb4737ca7970d08b6416050e224058328
CasianLW <ciorbacasian@gmail.com>
2022-12-15 09:34:23 +0100

    Generer 5 images a la fois

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const app = express();
 // console.log(123);
 //  Enable body parser
 app.use(express.json());
-app.use(express.urlencoded({ extennded: false }));
-app.use("/openai", require("./routes/openaiRoutes"));
+app.use(express.urlencoded({ extended: false }));
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/openai", require("./routes/openaiRoutes"));
 
 app.listen(port, () => console.log(`Server started on port: ${port}`));
--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,8 @@
-const { generateImage } = require("../controllers/openaiController");
+// const { generateImage } = require("./controllers/openaiController");
 
 function onSubmit(e) {
   e.preventDefault();
+  console.log("test");
   const prompt = document.querySelector("#prompt").value;
   const size = document.querySelector("#size").value;
   if (prompt === "") {
@@ -15,10 +16,11 @@ function onSubmit(e) {
 
 async function generateImageRequest(prompt, size) {
   try {
+    showSpinner();
     const response = await fetch("/openai/generateimage", {
-      method: POST,
+      method: "POST",
       headers: {
-        "Content-Type": "application/jsonn",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         prompt,
@@ -27,10 +29,29 @@ async function generateImageRequest(prompt, size) {
     });
     if (!response.ok) {
       removeSpinner();
+      console.log(prompt, size);
+      console.log(await response.json());
       throw new Error("L'image n'a pas pu être génerée");
     }
     const data = await response.json();
-  } catch (error) {}
+
+    removeSpinner();
+    // console.log(data);
+    // const imageUrl = data.data;
+    const imageUrl1 = data.data1;
+    const imageUrl2 = data.data2;
+    const imageUrl3 = data.data3;
+    const imageUrl4 = data.data4;
+    // console.log(imageUrl1);
+    console.log(await data);
+    document.querySelector("#image1").src = imageUrl1;
+    // document.querySelector("#image1").src = imageUrl1;
+    document.querySelector("#image2").src = imageUrl2;
+    document.querySelector("#image3").src = imageUrl3;
+    document.querySelector("#image4").src = imageUrl4;
+  } catch (error) {
+    document.querySelector(".msg").textContent = error;
+  }
 }
 
 function showSpinner() {
